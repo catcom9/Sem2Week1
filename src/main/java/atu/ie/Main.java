a7098f9a31853c5846ce7f3fff460b3ff743b4f1
Cathal Commins <g00427615@atu.ie>
2025-01-27 12:03:20 +0000

    Completed Totals

--- src/main/java/atu/ie/Main.java
+++ src/main/java/atu/ie/Main.java
@@ -8,10 +8,10 @@ public class Main {
         System.out.println("This is your order");
 
         //Create an order
-        Burger cheeseBurger = new Burger("Cheeseburger", 1.99, "Tasty");
-        Burger veggie = new Burger("Veggie Burger", 2.99, "Even Better");
-        Pizza Tuna = new Pizza("Tuna pizza", 3.43, "Has tuna on it");
-        Salad caesarSalad = new Salad("CaesarSald", 4.3, "Nothing");
+        Burger cheeseBurger = new Burger("Cheeseburger", 1.99, "Beef Patty, Bacon, Cheese");
+        Burger veggie = new Burger("Veggie Burger", 2.99, "Veggie Burger, Lettuce, tomato");
+        Pizza Tuna = new Pizza("Tuna pizza", 3.43, "Chesse, Tuna, Tomato Sause");
+        Salad caesarSalad = new Salad("Caesar Salad", 4.3, "Salad");
 
 
         //List all the food items like burgers, pizzas, fries, etc
@@ -20,11 +20,14 @@ public class Main {
         items.add(Tuna);
         items.add(caesarSalad);
 
+        double totalPrice = 0;
         for(MenuItem item : items){
             System.out.println("Name: " + item.getName());
             System.out.println("Ingredients: " + item.getDescription());
-            System.out.println("Price: " + item.getPrice());
+            System.out.println("Price: €" + item.getPrice() + "\n\n");
+            totalPrice += item.getPrice();
         }
+        System.out.println("Total order price is: €" + totalPrice);
 
 
 
